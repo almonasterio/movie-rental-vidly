f168a18f6632d36fd9850e4657c020f50e2ab76f
almonasterio <a.monasterio.a@gmail.com>
2020-09-08 07:55:08 -0500

    Pagination implemented

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./Pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    moviesPerPage:4,
+    pageNumber:1
+
   };
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
 
-  handleLike = movie => {
+  handleLike = (movie) => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
     movies[index] = { ...movies[index] };
@@ -20,11 +24,26 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  
+
+  handlePageChange = (e,pageNumber) => {
+    e.preventDefault();
+      this.setState({pageNumber: pageNumber})
+  };
+
+  display= ()=>{
+    const {pageNumber}=this.state;
+    const start= (pageNumber-1)*this.state.moviesPerPage;
+    const end= (pageNumber)*this.state.moviesPerPage;
+    return [...this.state.movies].slice(start,end)
+  }
+
+
   render() {
     const { length: count } = this.state.movies;
-
     if (count === 0) return <p>There are no movies in the database.</p>;
-
+    const displayMovies=this.display()
+    console.log("state,pageNumber", this.state.pageNumber);
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database.</p>
@@ -40,7 +59,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {displayMovies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -64,6 +83,7 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination onPageChange={this.handlePageChange} numberPages={Math.ceil(this.state.movies.length / this.state.moviesPerPage)} />
       </React.Fragment>
     );
   }
